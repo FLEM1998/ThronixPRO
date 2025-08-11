db6ab8d4573a58c01b18f13d2f422c03dfed8f0b
FLEM1998 <flemcoinsol1@gmail.com>
2025-08-11 19:36:22 +0100

    Delete ThronixPRO_unified directory

--- ThronixPRO_unified/ai_service/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ai_service
-python app.py
--- ThronixPRO_unified/attached_assets/schema_1754650816110.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE ai_bot_logs (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  user_id UUID NOT NULL,
-  coin TEXT NOT NULL,
-  strategy JSONB,
-  pnl FLOAT,
-  result TEXT,
-  timestamp TIMESTAMP DEFAULT now()
-);
